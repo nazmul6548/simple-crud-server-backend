ed7fa000b0830d6a8248a289ee3539931f67009e
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-04-22 00:21:01 +0600

    task

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const cors = require('cors')
-const {MongoClient,ServerApiVersion, ObjectId} =require('mongodb')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -11,16 +11,9 @@ app.use(express.json());
 // name:nazmul6548
 // password:DmQtLDdIBJH3MmtL
 
-
-
-
-
-
-
-
-
 // const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://nazmul6548:DmQtLDdIBJH3MmtL@cluster0.ce00xrg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri =
+  "mongodb+srv://nazmul6548:DmQtLDdIBJH3MmtL@cluster0.ce00xrg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,63 +21,77 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
     await client.connect();
 
     const database = client.db("usersDB");
     const userCollection = database.collection("users");
 
-
-    app.get("/users",async (req,res) => {
-        const cursor = userCollection.find()
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-
-    app.post('/users',async (req,res) => {
-        const user = req.body;
-        console.log('new user',user);
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-        console.log(result);
-    })
-    app.delete("/users/:id", async(req,res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await userCollection.deleteOne(query);
-        res.send(result);
-
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
-    
+
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("new user", user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+      console.log(result);
+    });
+
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateUser = req.body;
+      console.log(id, updateUser);
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedUser = {
+        $set: {
+          name: updateUser.name,
+          email: updateUser.email,
+        },
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updatedUser,
+        options
+      );
+      res.send(result);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    
     // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("simple crud is running");
+});
 
-
-
-
-
-
-
-
-
-
-
-app.get('/',(req,res) => {
-    res.send ('simple crud is running')
-})
-
-app.listen(port,() => {
-    console.log(`simple crud is listening,${port}`);
-})
+app.listen(port, () => {
+  console.log(`simple crud is listening,${port}`);
+});
